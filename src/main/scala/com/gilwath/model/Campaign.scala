c2c1bca4c59d7b73f525db5a60cdb8ff686c5fb4
Benjamin Thuillier <benjaminthuillier@gmail.com>
2014-06-26 17:43:31 +0200

    move logic from router to singleton campaigns

--- src/main/scala/com/gilwath/model/Campaign.scala
+++ src/main/scala/com/gilwath/model/Campaign.scala
@@ -2,11 +2,16 @@ package com.gilwath.model
 
 import reactivemongo.bson._
 import play.api.libs.json._
-import reactivemongo.bson.BSONString
+import scala.concurrent.Future
 import play.api.libs.json.JsSuccess
 import reactivemongo.bson.BSONInteger
+import reactivemongo.api.DefaultDB
+import play.api.libs.json.JsString
+import reactivemongo.bson.BSONString
 import play.api.data.validation.ValidationError
+import reactivemongo.api.collections.default.BSONCollection
 
+import scala.concurrent.ExecutionContext.Implicits._
 
 case class Campaign(id: Option[BSONObjectID], title: String, advertiser: String, volume: Int)
 
@@ -43,4 +48,26 @@ object Campaign {
       bson.getAs[Int]("volume").get
     )
   }
+}
+
+object Campaigns {
+  private def collection(implicit db: DefaultDB): BSONCollection = db("campaigns")
+
+  def delete(id: BSONObjectID)(implicit db: DefaultDB): Future[Unit] =
+    collection.remove(BSONDocument("_id" -> id)).map(_ => ())
+
+  def update(campaign: Campaign)(implicit db: DefaultDB): Future[Campaign] =
+    collection.update(BSONDocument("_id" -> campaign.id.get), campaign).map(_ => campaign)
+
+  def insert(campaign: Campaign)(implicit db: DefaultDB): Future[Campaign] = {
+    val c = campaign.copy(id = Some(BSONObjectID.generate))
+    collection.save(c).map(_ => c)
+  }
+
+  def findOneById(id: BSONObjectID)(implicit db: DefaultDB): Future[Option[Campaign]] = {
+    collection.find(BSONDocument("_id" -> id)).one[Campaign]
+  }
+
+  def findAll()(implicit db: DefaultDB): Future[List[Campaign]] =
+    collection.find(BSONDocument()).cursor[Campaign].collect[List]()
 }
